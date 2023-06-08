163c243266a964fc08e41e624da759f2b1daf6cb
Tran Sang <me@transa.ng>
2023-06-09 02:01:30 +0900

    es6

--- index.es.js
+++ index.es.js
@@ -1,2 +1,18 @@
-export default function jcls(...classNames: readonly (string | boolean | null | undefined | 0 | -.0 | /*typeof NaN |*/ 0n)[]): string
-export declare const rcls: (classNames: string, ...toRemove: readonly (string | boolean | 0 | 0n | RegExp | ((cls: string) => any) | null | undefined)[]) => string
+const classDelim = ' ';
+// https://github.com/Microsoft/TypeScript/issues/28682
+export default function jcls(...classNames) {
+	return classNames
+		.filter(Boolean)
+		.join(classDelim);
+}
+export const rcls = (classNames, ...toRemove) => {
+	const toRemoveList = toRemove.filter(Boolean);
+	return classNames
+		.split(classDelim)
+		.filter(cl => !toRemoveList.some(cls => typeof cls === 'string'
+			? cls === cl
+			: typeof cls === 'function'
+				? cls(cl)
+				: cls.test(cl)))
+		.join(classDelim);
+};
